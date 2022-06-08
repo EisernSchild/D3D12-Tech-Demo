be759d2b252fc7cc5c6198cc79d9ba57b7272bdc
Denis Reischl <eisernschild@gmail.com>
2022-06-08 19:21:15 +0200

    Game Timer and D3D12 application basics

--- D3D12 Tech Demo/app.h
+++ D3D12 Tech Demo/app.h
@@ -20,16 +20,170 @@
 #error "OS not supported!"
 #endif
 
+#ifdef _WIN32
+#define GameTimer game_timer
+
 /// <summary>
 /// Game Time simple helper.
 /// </summary>
-struct GameTime
+class game_timer
 {
-	double dTime;
+public:
+	/// <summary>get the frequency here</summary>
+	game_timer()
+	{
+		// get timer frequency
+		__int64 n64Frequency;
+		QueryPerformanceFrequency((LARGE_INTEGER*)&n64Frequency);
+		m_dTicksInSeconds = 1.0 / (double)n64Frequency;
+
+		// init timer
+		__int64 nCurrTime;
+		QueryPerformanceCounter((LARGE_INTEGER*)&nCurrTime);
+
+		m_nBaseTime = nCurrTime;
+		m_nPrevTime = nCurrTime;
+		m_nStopTime = 0;
+		m_bStopped = false;
+
+	}
+
+	/// <summary>Restart</summary>
+	void restart()
+	{
+		__int64 nStartTime;
+		QueryPerformanceCounter((LARGE_INTEGER*)&nStartTime);
+
+		if (m_bStopped)
+		{
+			m_nPausedTime += (nStartTime - m_nStopTime);
+
+			m_nPrevTime = nStartTime;
+			m_nStopTime = 0;
+			m_bStopped = false;
+		}
+	}
+	/// <summary>Pause</summary>
+	void pause()
+	{
+		if (!m_bStopped)
+		{
+			__int64 nCurrTime;
+			QueryPerformanceCounter((LARGE_INTEGER*)&nCurrTime);
+
+			m_nStopTime = nCurrTime;
+			m_bStopped = true;
+		}
+	}
+	/// <summary>New frame</summary>
+	void tick()
+	{
+		static int s_nFramesThisSecond = 0;
+		static int s_nFramesTotal = 0;
+		static float s_fSecondsCounter = 0.0f;
+
+		// increment frame counter for this second
+		s_nFramesThisSecond++;
+
+		// stopped ?
+		if (m_bStopped)
+		{
+			m_dDelta = 0.0;
+			m_fTotal = (float)(((m_nStopTime - m_nPausedTime) - m_nBaseTime) * m_dTicksInSeconds);
+			return;
+		}
+
+		// get counter
+		__int64 nCurrTime;
+		QueryPerformanceCounter((LARGE_INTEGER*)&nCurrTime);
+		m_nCurrTime = nCurrTime;
+
+		// set total
+		m_fTotal = (float)(((m_nCurrTime - m_nPausedTime) - m_nBaseTime) * m_dTicksInSeconds);
+
+		// set delta
+		m_dDelta = (m_nCurrTime - m_nPrevTime) * m_dTicksInSeconds;
+		m_fDelta = (float)m_dDelta;
+
+		// set previous frame time
+		m_nPrevTime = m_nCurrTime;
+
+		// clamp positive
+		if (m_dDelta < 0.0)	m_dDelta = 0.0;
+
+		// set fps if new second
+		if ((m_fTotal - s_fSecondsCounter) >= 1.0f)
+		{
+			// set total frames
+			s_nFramesTotal += s_nFramesThisSecond;
+
+			// calc fps
+			m_fFPSTotal = (float)s_nFramesThisSecond;
+			m_fFPS = s_nFramesThisSecond / (m_fTotal - s_fSecondsCounter);
+
+			// reset
+			s_nFramesThisSecond = 0;
+			s_fSecondsCounter += 1.0f;
+		}
+	}
+
+	/// <summary>Total time</summary>
+	float total() { return m_fTotal; }
+	/// <summary>Timer delta in seconds</summary>
+	float delta() { return m_fDelta; }
+	/// <summary>Frames per second, total</summary>
+	float fps_total() { return m_fFPSTotal; }
+	/// <summary>Frames per second</summary>
+	float fps() { return m_fFPS; }
+
+private:
+	/// <summary>Total time</summary>
+	float m_fTotal;
+	/// <summary>Timer delta in seconds</summary>
+	float m_fDelta;
+	/// <summary>Frames per second, total</summary>
+	float m_fFPSTotal;
+	/// <summary>Frames per second</summary>
+	float m_fFPS;
+	/// <summary>Ticks in seconds game timer helper</summary>
+	double m_dTicksInSeconds;
+	/// <summary>Timer delta (double)</summary>
+	double m_dDelta;
+	/// <summary>Timer base.</summary>
+	__int64 m_nBaseTime;
+	/// <summary>Timer pause time helper.</summary>
+	__int64 m_nPausedTime;
+	/// <summary>Timer stop time helper.</summary>
+	__int64 m_nStopTime;
+	/// <summary>Timer previous time helper.</summary>
+	__int64 m_nPrevTime;
+	/// <summary>Timer current time helper.</summary>
+	__int64 m_nCurrTime;
+	/// <summary>True if the timer is stopped.</summary>
+	bool m_bStopped;
 };
 
-#define FUNC_GAME_TASK(task) static signed task(GameTime& sInfo)
-typedef signed(*GAME_TASK)(GameTime& sInfo);
+#else 
+#error "OS not supported!"
+#endif
+
+/// <summary>
+/// Global application data.
+/// </summary>
+struct AppData
+{
+	/// <summary>Total time</summary>
+	float fTotal;
+	/// <summary>Timer delta in seconds</summary>
+	float fDelta;
+	/// <summary>Frames per second, total</summary>
+	float fFPSTotal;
+	/// <summary>Frames per second</summary>
+	float fFPS;
+};
+
+#define FUNC_GAME_TASK(task) static signed task(AppData& sInfo)
+typedef signed(*GAME_TASK)(AppData& sInfo);
 
 /// <summary>
 /// App Basics Parent Class.
@@ -67,7 +221,7 @@ protected:
 	{
 		// pointer helpers for following operation
 		std::vector<std::vector<GAME_TASK>>* paav = nullptr;
-		std::vector<std::vector<std::packaged_task<signed(GameTime&)>>>* paac = nullptr;
+		std::vector<std::vector<std::packaged_task<signed(AppData&)>>>* paac = nullptr;
 
 		// create lists of task blocks
 		for (unsigned uIx : {INIT, RUNTIME, DESTROY})
@@ -77,15 +231,15 @@ protected:
 			{
 			case INIT:
 				paav = &aavTasks_Init;
-				paac = &aacTasks_Init;
+				paac = &m_aacTasks_Init;
 				break;
 			case RUNTIME:
 				paav = &aavTasks_Runtime;
-				paac = &aacTasks_Runtime;
+				paac = &m_aacTasks_Runtime;
 				break;
 			case DESTROY:
 				paav = &aavTasks_Destroy;
-				paac = &aacTasks_Destroy;
+				paac = &m_aacTasks_Destroy;
 				break;
 			default: continue; break;
 			}
@@ -95,9 +249,9 @@ protected:
 			{
 
 				// create task block and add to list
-				std::vector<std::packaged_task<signed(GameTime&)>> acTaskblock;
+				std::vector<std::packaged_task<signed(AppData&)>> acTaskblock;
 				for (GAME_TASK& v : av)
-					acTaskblock.push_back(std::packaged_task<signed(GameTime&)>(v));
+					acTaskblock.push_back(std::packaged_task<signed(AppData&)>(v));
 
 				// add the block at begin (?)
 				if (bInsertFirst)
@@ -116,9 +270,12 @@ protected:
 	/// </summary>
 	void Run()
 	{
-		GameTime sTime = {};
-		std::vector<std::vector<std::packaged_task<signed(GameTime&)>>>* paac = nullptr;
+		AppData sAppData = {};
+		std::vector<std::vector<std::packaged_task<signed(AppData&)>>>* paac = nullptr;
 		signed nFuture = APP_FORWARD;
+
+		// restart the game timer
+		m_cTimer.restart();
 
 		// execute lists of task blocks
 		for (unsigned uIx : {INIT, RUNTIME, DESTROY})
@@ -129,16 +286,23 @@ protected:
 			// set helper pointer
 			switch (uIx)
 			{
-			case INIT: paac = &aacTasks_Init; break;
-			case RUNTIME: paac = &aacTasks_Runtime; break;
-			case DESTROY: paac = &aacTasks_Destroy; break;
+			case INIT: paac = &m_aacTasks_Init; break;
+			case RUNTIME: paac = &m_aacTasks_Runtime; break;
+			case DESTROY: paac = &m_aacTasks_Destroy; break;
 			default: continue; break;
 			}
 
 			do
 			{
+				// update game timer and app data
+				m_cTimer.tick();
+				sAppData.fDelta = m_cTimer.delta();
+				sAppData.fFPS = m_cTimer.fps();
+				sAppData.fFPSTotal = m_cTimer.fps_total();
+				sAppData.fTotal = m_cTimer.total();
+
 				// loop through task blocks
-				for (std::vector<std::packaged_task<signed(GameTime&)>>& aac : *paac)
+				for (std::vector<std::packaged_task<signed(AppData&)>>& aac : *paac)
 				{
 					// pause this task block ?
 					if (nFuture == APP_PAUSE) continue;
@@ -148,14 +312,14 @@ protected:
 
 					// loop through tasks
 					std::vector<std::future<signed>> acFutures;
-					for (std::packaged_task<signed(GameTime&)>& ac : aac)
+					for (std::packaged_task<signed(AppData&)>& ac : aac)
 					{
 						// reset task, add future
 						ac.reset();
 						acFutures.push_back(ac.get_future());
 
 						// execute
-						ac(sTime);
+						ac(sAppData);
 					}
 
 					// loop through futures to synchronize
@@ -194,12 +358,17 @@ private:
 	/// Each vector of tasks (=taskblock) will be executed simultanely.
 	/// Each vector of vectors will be executed sequentually.
 	/// </summary>
-	std::vector<std::vector<std::packaged_task<signed(GameTime&)>>>
-		aacTasks_Init, aacTasks_Runtime, aacTasks_Destroy;
+	std::vector<std::vector<std::packaged_task<signed(AppData&)>>>
+		m_aacTasks_Init, m_aacTasks_Runtime, m_aacTasks_Destroy;
+	/// <summary>
+	/// Game Timer (platform dependent)
+	/// </summary>
+	GameTimer m_cTimer;
 };
 
 #ifdef _WIN32
-#define APP App_Windows
+#define APP_Os App_Windows
+#define APP_GfxLib App_D3D12
 
 /// <summary>
 /// Windows app skeleton
@@ -211,19 +380,7 @@ public:
 		std::vector<std::vector<GAME_TASK>>& aavTasks_Init,
 		std::vector<std::vector<GAME_TASK>>& aavTasks_Runtime,
 		std::vector<std::vector<GAME_TASK>>& aavTasks_Destroy
-	) : App_Taskhandler(aavTasks_Init, aavTasks_Runtime, aavTasks_Destroy)
-	{
-		// create and add the task function blocks for any Windows action
-		std::vector<std::vector<GAME_TASK>> aavTasks_Init_this = { { OsInit } };
-		std::vector<std::vector<GAME_TASK>> aavTasks_Runtime_this = { { OsFrame }, { OsUpdate } };
-		std::vector<std::vector<GAME_TASK>> aavTasks_Destroy_this = { { OsPreRelease } };
-		Add(aavTasks_Init_this, aavTasks_Runtime_this, aavTasks_Destroy_this, true);
-		aavTasks_Destroy_this = { { OsRelease } };
-		Add(aavTasks_Init_this, aavTasks_Runtime_this, aavTasks_Destroy_this, false);
-
-		// .. and execute the app
-		Run();
-	}
+	) : App_Taskhandler(aavTasks_Init, aavTasks_Runtime, aavTasks_Destroy) {}
 	virtual ~App_Windows() {}
 
 protected:
@@ -233,6 +390,7 @@ protected:
 	/// </summary>
 	FUNC_GAME_TASK(OsInit)
 	{
+		OutputDebugStringA("App_Windows::OsInit");
 		HINSTANCE pHInstance = GetModuleHandle(NULL);
 
 		// register window class
@@ -304,26 +462,26 @@ protected:
 	/// <summary>Windows main procedure</summary>
 	static LRESULT CALLBACK WindowProc(HWND pHwnd, UINT uMessage, WPARAM uWparam, LPARAM uLparam)
 	{
-		GameTime* psTime = reinterpret_cast<GameTime*>(GetWindowLongPtr(pHwnd, GWLP_USERDATA));
+		AppData* psData = reinterpret_cast<AppData*>(GetWindowLongPtr(pHwnd, GWLP_USERDATA));
 
 		switch (uMessage)
 		{
 		case WM_CREATE:
 		{
-			// Save the DXSample* passed in to CreateWindow.
+			// s ave the app data pointer send via CreateWindow
 			LPCREATESTRUCT pCreateStruct = reinterpret_cast<LPCREATESTRUCT>(uLparam);
 			SetWindowLongPtr(pHwnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(pCreateStruct->lpCreateParams));
 		}
 		return 0;
 
 		case WM_KEYDOWN:
-			if (psTime)
+			if (psData)
 			{
 			}
 			return APP_FORWARD;
 
 		case WM_KEYUP:
-			if (psTime)
+			if (psData)
 			{
 			}
 			return APP_FORWARD;
@@ -342,6 +500,37 @@ protected:
 
 HWND App_Windows::m_pHwnd = nullptr;
 
+/// <summary>
+/// D3D12 application
+/// </summary>
+class App_D3D12 : protected App_Windows
+{
+public:
+	explicit App_D3D12(
+		std::vector<std::vector<GAME_TASK>>& aavTasks_Init,
+		std::vector<std::vector<GAME_TASK>>& aavTasks_Runtime,
+		std::vector<std::vector<GAME_TASK>>& aavTasks_Destroy
+	) : App_Windows(aavTasks_Init, aavTasks_Runtime, aavTasks_Destroy) {}
+	virtual ~App_D3D12() {}
+
+protected:
+
+	/// <summary>
+	/// Init D3D12
+	/// </summary>
+	FUNC_GAME_TASK(GxInit)
+	{
+		OutputDebugStringA("App_D3D12::GxInit");
+		return APP_FORWARD;
+	}
+	/// <summary></summary>
+	FUNC_GAME_TASK(GxRelease)
+	{
+		OutputDebugStringA("App_D3D12::GxRelease");
+		return APP_FORWARD;
+	}
+};
+
 #else
 #error "OS not supported!"
 #endif
@@ -349,10 +538,14 @@ HWND App_Windows::m_pHwnd = nullptr;
 /// <summary>
 /// Main Application
 /// </summary>
-class App : protected APP
+class App : protected APP_GfxLib
 {
 public:
-	App() : APP(m_aavTasks_Init, m_aavTasks_Runtime, m_aavTasks_Destroy) {}
+	App() : APP_GfxLib(m_aavTasks_Init, m_aavTasks_Runtime, m_aavTasks_Destroy) 
+	{
+		// .. and execute the app
+		Run();
+	}
 	~App() {}
 
 	FUNC_GAME_TASK(OnInit)
@@ -395,10 +588,13 @@ public:
 	static std::vector<std::vector<GAME_TASK>> m_aavTasks_Destroy;
 };
 
-std::vector<std::vector<GAME_TASK>> App::m_aavTasks_Init = { { App::OnInit } };
-std::vector<std::vector<GAME_TASK>> App::m_aavTasks_Runtime = { 
-	{ App::OnUpdate }, 
+/// The concurrent scheme of the App 
+std::vector<std::vector<GAME_TASK>> App::m_aavTasks_Init = { { APP_Os::OsInit }, { APP_GfxLib::GxInit },  { App::OnInit } };
+std::vector<std::vector<GAME_TASK>> App::m_aavTasks_Runtime = {
+	{ App::OnUpdate, APP_Os::OsUpdate }, { APP_Os::OsFrame },
 	{ App::OnRenderPipeline0, App::OnRenderPipeline1, App::OnRenderPipeline2, App::OnRenderAudio },
 	{ App::OnPostRender }
 };
-std::vector<std::vector<GAME_TASK>> App::m_aavTasks_Destroy = { { App::OnRelease } };
+std::vector<std::vector<GAME_TASK>> App::m_aavTasks_Destroy = { 
+	{ APP_Os::OsPreRelease }, { APP_GfxLib::GxRelease }, { App::OnRelease }, { APP_Os::OsRelease }
+};
